d962041b9c7d0a4414a3967869af5505175a29d2
pierobramanti <piero.bramanti94@gmail.com>
2024-07-04 17:24:00 +0200

    [UPD] opened function

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,3 +1,10 @@
 // DICHIARO UNA VARIABILE A CUI ASSEGNO METODO PROMPT//
 
- let UserChoice = prompt("Scrivi pari o dispari").toLowerCase();
+ let userChoice = prompt("Scrivi pari o dispari").toLowerCase();
+
+// DICHIARO UNA SECONDA VARIABILE PER INSERIRE UN NUMERO//
+ let userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
+ function pcNumber(){
+    return Math.floor(Math.random() * 5) + 1;
+ }

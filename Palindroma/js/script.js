d04e61bd525e8b5ca53b47329a5209a63297da44
pierobramanti <piero.bramanti94@gmail.com>
2024-07-04 15:59:54 +0200

    [UPD] inserted IF

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -3,4 +3,9 @@ function palindrome (word){
 // UTILIZZO IL METODO SPLIT PER CREARE UN ARRAY CON SINGOLE LETTERE, CON REVERSE INVERTO GLI ORDINI DEGLI ELEMENTI E USO JOIN PER UNIRLI
     let reverse_word = word.split("").reverse("").join("");
 
+//INSERISCO IF
+
+if (word===reverse_word)
+    console.log(`la parola "${word}" è palindroma`)
+
 }

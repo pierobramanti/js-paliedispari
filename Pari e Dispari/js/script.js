9a2aee1fac1350e80c5d56afa898a17c9713327e
pierobramanti <piero.bramanti94@gmail.com>
2024-07-04 17:40:35 +0200

    [UPD] inserted method math.floor(math.random) to generate pc choice

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -2,9 +2,17 @@
 
  let userChoice = prompt("Scrivi pari o dispari").toLowerCase();
 
+
 // DICHIARO UNA SECONDA VARIABILE PER INSERIRE UN NUMERO//
  let userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
 
  function pcNumber(){
     return Math.floor(Math.random() * 5) + 1;
  }
+
+ let pcChoice = pcNumber;
+
+ let sum = userNumber + userNumber;
+ console.log(sum)
+
+
